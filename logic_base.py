c3e3862ce2e87bc711b6479d81cec21f2b7f629a
PitiPipouSauvage <96929981+PitiPipouSauvage@users.noreply.github.com>
2022-07-02 21:29:46 +0100

    Update 0.2
    The encoding part is completed but the decode part is still work in progress

--- logic_base.py
+++ logic_base.py
@@ -1,17 +1,20 @@
 from random import randint
 
-Alphabet = {"a" : 1, "b" : 2, "c" : 3, "d" : 4, "e" : 5, "f" : 6, "g" : 7, "h" : 8, "i" : 9, "j" : 10, "k" : 11, "l" : 12, "m" : 13, "n" : 14, "o" : 15, "p" : 16, "q" : 17, "r" : 18, "s" : 19, "t" : 20, "u" : 21, "v" : 22, "w" : 23, "x" : 24, "y" : 25, "z" : 26}
+Alphabet = {"a" : 1, "b" : 2, "c" : 3, "d" : 4, "e" : 5, "f" : 6, "g" : 7, "h" : 8, "i" : 9, "j" : 10, "k" : 11, "l" : 12, "m" : 13, "n" : 14, "o" : 15, "p" : 16, "q" : 17, "r" : 18, "s" : 19, "t" : 20, "u" : 21, "v" : 22, "w" : 23, "x" : 24, "y" : 25, "z" : 26, " " : 27}
 
-ListOfKeys = list(Alphabet.keys()) #Save of the Alphabet keys 
-ListOfValues = list(Alphabet.values()) #Save of the Alphabet values
+ListOfKeys = list(Alphabet.keys()) #Save the Alphabet dict keys 
+ListOfValues = list(Alphabet.values()) #Save the Alphabet dict values
 
-CodeKey = list()
+CodeKeyList = list()
+CodeKey = int()
+
+MessageFList = list()
 
 class rotors:
     def __init__(self, NumberOfRotation):
         self.NumberOfRotation = NumberOfRotation
         self.StartingLetter = int() #integer corresponding at the ID of the letter in the dict 
-        self.Message = input("Enter a message ")
+        self.Message = str()
         self.LetterID = int()
         self.NewLetterID = int()
         self.NewLetterKey = str()
@@ -21,15 +24,26 @@ class rotors:
         self.MessageNF = str()
         self.MessageF = str()
         self.NewLetterID2 = int()
+        self.key = str()
+        self.keyList = list()
+        self.OldLetter = str()
+        self.OldLetterID = int()
+        self.OldMessageList = list()
+        self.OldMessage = str()
         
     def encode(self):
+        self.Message = input("Enter a message ")
         self.MessageNF = self.Message 
         MessageList = list()
         for j in range(self.NumberOfRotation):
-            self.StartingLetterID = randint(0, 25)
-            self.StartingLetter = ListOfKeys[self.StartingLetterID]
-            self.StartID = Alphabet[self.StartingLetter]
+            #Definition the ammount of rotation
+            self.StartingLetterID = randint(0, 26)  
+            # self.StartingLetter = ListOfKeys[self.StartingLetterID]
+            # self.StartID = Alphabet[self.StartingLetter]
+            
             for i in self.MessageNF:
+                self.StartingLetter = ListOfKeys[self.StartingLetterID]
+                self.StartID = Alphabet[self.StartingLetter]
                 self.LetterID = Alphabet[i]
                 self.NewLetterIDNF = self.LetterID + self.StartID
                 self.NewLetterIDNF2 = self.NewLetterIDNF - 26
@@ -37,14 +51,45 @@ class rotors:
                 self.NewLetterKey = ListOfKeys[self.NewLetterID - 1]
                 self.NewLetter = self.NewLetterKey 
                 MessageList.append(self.NewLetter)
-                CodeKey.append(self.StartingLetterID)   #create a key to decode the message 
-                self.StartingLetterID += 1 
+                CodeKeyList.append(str(self.StartingLetterID))   #create a key to decode the message 
+                if self.StartingLetterID < 26:
+                    self.StartingLetterID += 1
+                else:
+                    self.StartingLetterID = 0 
             
+            #Front end part         
             self.MessageF = "".join(MessageList)
-            print(self.MessageF)
-            self.MessageNF = self.MessageF 
+            self.MessageNF = self.MessageF
+            MessageFList.append(self.MessageF) 
             self.MessageF = None 
             self.MessageF = str()
             MessageList = None 
             MessageList = list()
+        
+        # for i in range(self.NumberOfRotation - 1):
+        #     MessageFList.pop()
+        
+        self.MessageF = MessageFList[-1]
+        CodeKey = "/".join(CodeKeyList)
         print(self.MessageF)
+        print(CodeKey)
+    
+    def decode(self):
+        self.NumberOfRotation = int(input("Number of rotation: ")) 
+        self.Message = input("Enter the message you want to decode: ")
+        self.key = input("Enter the key to decrypt: ")
+        self.keyList = self.key.split("/")
+        for i in range(self.NumberOfRotation):
+            for j in self.Message:
+                self.OldLetterID = Alphabet[j] - self.keyList[-i]
+                if self.OldLetterID < 0:
+                    self.OldLetterID += 27
+                else:
+                    self.OldLetterID = self.OldLetterID 
+                
+                self.OldLetter = ListOfValues[self.OldLetterID - 1] 
+                self.OldMessageList.append(self.OldLetter)
+                print(self.OldMessageList)
+        self.OldMessage = "".join(self.OldMessageList) 
+        print(self.OldMessage)      
+        print(self.OldMessageList)       
